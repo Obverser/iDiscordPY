8be054e6c0a1c0a0b734fbee60fec2f55eba4609
Gabriel (Obverser) <57118333+Obverser@users.noreply.github.com>
2021-07-14 15:36:40 -0700

    Initial Commit

---
+++ Shared/iDiscordPYApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDiscordPYApp.swift
+//  Shared
+//
+//  Created by Gabriel Linecker on 7/14/21.
+//
+
+import SwiftUI
+
+@main
+struct iDiscordPYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
