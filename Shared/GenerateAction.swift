bd4531699e6225d9f85ebcf25d41c5ea908379e8
Gabriel (Obverser) <57118333+Obverser@users.noreply.github.com>
2021-07-16 19:03:09 -0700

    Initial Commit, switching from vanilla Swift w/ Sword to Swift w/ PythonKit and Discord.py

---
+++ Shared/GenerateAction.swift
@@ -0,0 +1,8 @@
+//
+//  GenerateAction.swift
+//  iDiscordPY
+//
+//  Created by Gabriel Linecker on 7/16/21.
+//
+
+import Foundation
--- Shared/iDiscordPYApp.swift
+++ Shared/iDiscordPYApp.swift
@@ -6,12 +6,43 @@
 //
 
 import SwiftUI
+import PythonKit
+#if os(iOS)
+import PythonSupport
+#endif
 
 @main
 struct iDiscordPYApp: App {
     var body: some Scene {
         WindowGroup {
+            let _ = py.shared
             ContentView()
         }
+    }
+}
+
+class py {
+    // Singleton decleration
+    static let shared = py()
+    let discord: PythonObject
+    let sys: PythonObject
+    let os: PythonObject
+    private init() {
+        #if os(iOS)
+            PythonSupport.initialize()
+        #else
+            PythonLibrary.useVersion(3)
+        #endif
+        sys = Python.import("sys")
+        os = Python.import("os")
+        #if os(iOS)
+            sys.path.append(Bundle.main.bundlePath)
+        #else
+            sys.path.append(Bundle.main.bundlePath + "/Contents/Resources")
+        #endif
+        discord = Python.import("discord")
+        print("Finished 'init', discord.py loaded.")
+        print(Python.version)
+        print(sys.path)
     }
 }
